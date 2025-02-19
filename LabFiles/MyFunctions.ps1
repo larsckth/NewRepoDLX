650ffff9137c9f4e444ff090dc16d800e9a7d1aa
Lasse-KTH <larsc@kth.se>
2025-02-19 10:23:50 +0100

    Code changes in MyFunctions. Yay!

---
+++ LabFiles/MyFunctions.ps1
@@ -0,0 +1,113 @@
+
+enum ColorEnum {
+    red
+    green
+    blue
+    yellow
+}
+
+class User {
+    [string] $Name
+    [int] $Age
+    [ColorEnum] $Color 
+#    [int] $Id
+    [int] $UserID
+    
+    User([String]$Name, [int]$Age, [ColorEnum]$Color, [int]$UserId) {
+        $This.Name = $Name
+        $This.Age = $Age
+        $This.Color = $Color
+        $This.Id = $UserId
+     #   $This.Id = $Id
+        $This.Id = $UserId
+    }
+
+    [string] ToString() {
+        Return '{0},{1},{2},{3}' -f $This.Name, $This.Age, $This.Color, $This.Id
+    }
+
+}
+  function GetUserData {
+    $MyUserListFile = "$PSScriptRoot\MyLabFile.csv"
+    $MyUserList = Get-Content -Path $MyUserListFile | ConvertFrom-Csv
+    $MyUserList
+}
+
+function Get-CourseUser {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]$Name,
+        
+        [Parameter()]
+        [int]$OlderThan
+    )
+
+    $Result = GetUserData
+
+    if (-not [string]::IsNullOrEmpty($Name)) {
+        $Result = $Result | Where-Object -Property Name -Like "*$Name*"
+    }
+    
+    if ($OlderThan) {
+        $Result = $Result | Where-Object -Property Age -ge $OlderThan
+    }
+
+    $Result
+}
+
+function Add-CourseUser {
+    [CmdletBinding()]
+    Param (
+        $DatabaseFile = "$PSScriptRoot\MyLabFile.csv",
+
+        [Parameter(Mandatory)]
+        [string]$Name,
+
+        [Parameter(Mandatory)]
+        [Int]$Age,
+
+    #    [Parameter(Mandatory)]
+    #    [ValidateSet('red', 'green', 'blue', 'yellow')]
+    #    [string]$Color,
+
+        [Parameter(Mandatory)]
+        [ColorEnum]$Color,
+
+        $UserID = (Get-Random -Minimum 10 -Maximum 100000)
+    )
+    # $MyCsvUser = "$Name,$Age,{0},{1}" -f $Color, $UserId
+   
+    $MyNewUser = [User]::new($Name, $Age, $Color, $UserId)
+    $MyCsvUser = $MyNewUser.ToString() 
+
+    $NewCSv = Get-Content $DatabaseFile -Raw
+    $NewCSv += $MyCsvUser
+
+    Set-Content -Value $NewCSv -Path $DatabaseFile
+}
+
+function Remove-CourseUser {
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact='High')]
+    Param (
+        $DatabaseFile = "$PSScriptRoot\MyLabFile.csv"
+    )
+
+    $MyUserList = Get-Content -Path $DatabaseFile | ConvertFrom-Csv
+    $RemoveUser = $MyUserList | Out-ConsoleGridView -OutputMode Single
+        
+    if ($PSCmdlet.ShouldProcess([string]$RemoveUser.Name)) {
+        $MyUserList = $MyUserList | Where-Object {
+            -not (
+                $_.Name -eq $RemoveUser.Name -and
+                $_.Age -eq $RemoveUser.Age -and
+                $_.Color -eq $RemoveUser.Color -and
+                $_.Id -eq $RemoveUser.Id
+            )
+        }
+        Set-Content -Value $($MyUserList | ConvertTo-Csv -notypeInformation) -Path $MyUserListFile -WhatIf
+    }
+    else {
+        Write-Output "Did not remove user $($RemoveUser.Name)"
+    }
+}
